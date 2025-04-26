ca2d04488ac4ea371c3e1e541168d74f412d34bf
vaibhavgarg25 <gargvaibhav1225@gmail.com>
2025-04-26 21:19:05 +0530

    few more changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const errormiddleware=require('./middlewares/error-middleware')
 const cors=require('cors')
 
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://split-wise-ai.vercel.app/",
     method:"POST,GET,PUT,DELETE,PATCH,HEAD",
     credentials:true,
 }
