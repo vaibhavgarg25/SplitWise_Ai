1ad137da9790ce6d94281b261931df9cc6239a6e
vaibhavgarg25 <gargvaibhav1225@gmail.com>
2025-04-26 21:22:48 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
-require('dotenv').config()
-const express=require('express')
-const app=express()
-const port=3000
-const routes=require('./routes')
-const connectdb=require('./utils/db')
-const errormiddleware=require('./middlewares/error-middleware')
-const cors=require('cors')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const routes = require('./routes');
+const connectdb = require('./utils/db');
+const errormiddleware = require('./middlewares/error-middleware');
+const cors = require('cors');
 
-const corsOptions={
-    origin:"https://split-wise-ai.vercel.app/",
-    method:"POST,GET,PUT,DELETE,PATCH,HEAD",
-    credentials:true,
-}
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use('/routes',routes)
-app.use(errormiddleware)
+const corsOptions = {
+    origin: "https://split-wise-ai.vercel.app", 
+    methods: "POST,GET,PUT,DELETE,PATCH,HEAD", 
+    credentials: true,
+};
 
-connectdb().then(()=>{
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/routes', routes);
+app.use(errormiddleware);
+
+connectdb().then(() => {
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-    })
-  })
+        console.log(`Example app listening on port ${port}`);
+    });
+});
