bc340f09a93ea107286e6a4fbe4e8577f599b2e4
vaibhavgarg25 <gargvaibhav1225@gmail.com>
2025-04-27 11:15:28 +0530

    cors TT

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 3000;
+const cors = require('cors');
 const routes = require('./routes');
 const connectdb = require('./utils/db');
 const errormiddleware = require('./middlewares/error-middleware');
-const cors = require('cors');
+
+const app = express();
+const port = 3000;
 
 const corsOptions = {
   origin: ["https://split-wise-ai.vercel.app"],
-  methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "HEAD"], 
+  methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "HEAD"],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); 
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use('/routes', routes);
 app.use(errormiddleware);
 
 connectdb().then(() => {
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 });
